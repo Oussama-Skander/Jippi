9c01411a18181cadf8a1045305282eab791c76f4
Oussama-Skander <oussa.skan1992@gmail.com>
2020-10-15 14:17:40 +0100

    first components sturcture

--- Jippi/src/app/app.module.ts
+++ Jippi/src/app/app.module.ts
@@ -3,10 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustNavbarComponent } from './User/Customer/cust-navbar/navbar.component';
+import { HomeComponent } from './User/Customer/home/home.component';
+import { FilterComponent } from './User/Customer/home/filter/filter.component';
+import { AuthentificationComponent } from './User/customer/authentification/authentification.component';
+import { SignupComponent } from './User/customer/authentification/signup/signup.component';
+import { LoginComponent } from './User/customer/authentification/login/login.component';
+import { ContactUsComponent } from './User/customer/contact-us/contact-us.component';
+import { ItemsComponent } from './User/Customer/home/items/items.component';
+import { ItemComponent } from './User/Customer/home/items/item/item.component';
+import { CompNavbarComponent } from './user/company/comp-navbar/comp-navbar.component';
+import { ShopProfileComponent } from './user/company/shop-profile/shop-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustNavbarComponent,
+    HomeComponent,
+    FilterComponent,
+    ItemComponent,
+    AuthentificationComponent,
+    SignupComponent,
+    LoginComponent,
+    ContactUsComponent,
+    ItemsComponent,
+    ItemComponent,
+    CompNavbarComponent,
+    ShopProfileComponent
   ],
   imports: [
     BrowserModule,
