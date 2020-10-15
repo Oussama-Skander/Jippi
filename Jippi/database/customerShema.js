f3eb64cc4a85909d6a6cc979050551d5469bacbf
ezzineog <ezzineahmedtaha@gmail.com>
2020-10-16 00:29:38 +0200

    Sequelize / mySql connection

--- Jippi/database/customerShema.js
+++ Jippi/database/customerShema.js
@@ -2,18 +2,16 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("sqlite::memory");
 
 const Customer = sequelize.define(
-  "Customer",
-  {
+  "Customer", {
     first_Name: sequelize.STRING,
     last_Name: sequelize.STRING,
     email: sequelize.STRING,
     password: sequelize.STRING,
     avatar: sequelize.STRING,
-    adress: sequelize.STRING,
-  },
-  {
+    adress: sequelize.STRING
+  }, {
     tableName: "Customer_table", // this will define the table's name
   }
 );
 
-module.exports =  Customer;
+module.exports = Customer;
--- Jippi/server/routes/auth/customer/signupcustomer.js
+++ Jippi/server/routes/auth/customer/signupcustomer.js
@@ -1,1 +1,11 @@
 const signupCustomerRouter = require("express").Router();
+const Customer = require("../../../../database/customerShema.js");
+
+signupCustomerRouter.post("/signup", (req, res) => {
+  try {
+    const result = await Customer.create({});
+    res.send(result);
+  } catch (err) {
+    res.send(`signupCustomerRouter err ${err}`);
+  }
+});
